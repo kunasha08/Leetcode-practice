25153b409ddd2b48f2f4dcc51760466ea32f4275
Anushka Gupta <115527960+kunasha08@users.noreply.github.com>
2025-05-21 13:24:16 +0530

    Update solutions to LC problems

--- Java/RightRotateK.java
+++ Java/RightRotateK.java
@@ -2,9 +2,40 @@ package com.pkg.ag2.arr;
 
 public class RightRotateK {
 
-	public static void main(String[] args) {
+	static void rotateArr(int[] arr, int d) {
 		
+		int n =arr.length;
+		d=d%n;
+		
+		int[] res= new int[n];
+		
+		for (int i=0; i<d; i++) {
+			res[i]= arr[n-d+i];
+		}
+		
+		for (int i=0; i<n-d; i++) {
+			res[d+i]= arr[i];
+		}
+		
+        for (int i = 0; i < n; i++) {         //to make input array the result array
+            arr[i] = res[i];
+        }
+        
+        for (int k = 0; k < arr.length; k++)
+            System.out.print(arr[k] + " ");			//print result array
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6,7};
+        int d = 2;
+
+        rotateArr(arr, d);	
 
 	}
+
+
 
 }
